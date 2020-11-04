ae9780b28ec50e74ee05f5e60542290a4fd9b48d
Utha89 <mcrclaudio08@gmail.com>
2020-11-04 20:57:30 +0700

    Membuat variabel isi func

---
+++ go_web_variabel_isi_func/go.mod
@@ -0,0 +1,3 @@
+module go_web_variabel_isi_func
+
+go 1.15
---
+++ go_web_variabel_isi_func/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main()  {
+	mux:=http.NewServeMux()
+
+	//Membuat variabel isi func
+	data:=func(w http.ResponseWriter, r*http.Request){
+		//body dr fnctionnya 
+	w.Write([]byte("about"))
+	}
+
+	//mux.HandleFunc
+	// func(pattern string, handler func(ResponseWriter, *Request))
+	//pattern string isi /hello
+	mux.HandleFunc("/",homeHandler)
+	mux.HandleFunc("/hello",helloHandler)
+	mux.HandleFunc("/mario",marioHandler)
+	//panggil variabel isi func
+	mux.HandleFunc("/about",data)
+
+	
+	log.Println("Starting web on port 8080")
+
+	//jalanin servernya 
+	//LListenAndServe func(addr string, handler Handler) error
+	//parameter pertamanya addr string
+	//keduanya si handle mux itu sendiri
+	err:=http.ListenAndServe(":8080", mux)
+	log.Fatal(err)
+}
+//funtion baru yg membutuhkan 2 paramater yaitu 
+//1. w http.ResponseWriter
+//2. pointer nya yaitu r*http.Request
+func helloHandler(w http.ResponseWriter, r*http.Request)  {
+	//body dr fnctionnya 
+	w.Write([]byte("Lagi Belajar Go Lang guys"))
+}
+
+func marioHandler(w http.ResponseWriter, r*http.Request)  {
+	//body dr fnctionnya 
+	w.Write([]byte("Mario Balotelli"))
+}
+
+func homeHandler(w http.ResponseWriter, r*http.Request)  {
+	//body dr fnctionnya 
+	
+	//ngehandle 4040
+	log.Println(r.URL.Path)
+	if r.URL.Path !="/"{
+		http.NotFound(w ,r)
+		return
+	}
+	w.Write([]byte("Home Page"))
+}
