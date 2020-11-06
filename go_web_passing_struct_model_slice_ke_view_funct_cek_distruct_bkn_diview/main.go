48fa1cd614cdb63f07f5aa7761939fdd6d111571
Utha89 <mcrclaudio08@gmail.com>
2020-11-07 03:30:37 +0700

    tambhin load file css di folder assest

--- go_web_passing_struct_model_slice_ke_view_funct_cek_distruct_bkn_diview/main.go
+++ go_web_passing_struct_model_slice_ke_view_funct_cek_distruct_bkn_diview/main.go
@@ -9,13 +9,17 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
-	//mux.HandleFunc
+	//mux.HandleFunc atau rooting halaman
 	// func(pattern string, handler func(ResponseWriter, *Request))
 	//pattern string isi /hello
 	mux.HandleFunc("/", handler.HomeHandler)
 	mux.HandleFunc("/hello", handler.HelloHandler)
 	mux.HandleFunc("/mario", handler.MarioHandler)
 	mux.HandleFunc("/product", handler.ProductHandler)
+
+	//cara load file css di folder assets
+	fileServer := http.FileServer(http.Dir("assets"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	log.Println("Starting web on port 8080")
 
